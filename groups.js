1873aecac18ba03602aa93f4c0d41d5ca1163154
Bartek <bartekbohdziewicz@gmail.com>
2022-10-19 22:33:53 +0200

    Groups in one separate file

---
+++ groups.js
@@ -0,0 +1,158 @@
+function ShowGroupA(){
+    let grpA = document.querySelector('#groupA');
+    let grpB = document.querySelector('#groupB');
+    let grpC = document.querySelector('#groupC');
+    let grpD = document.querySelector('#groupD');
+    let grpE = document.querySelector('#groupE');
+    let grpF = document.querySelector('#groupF');
+    let grpG = document.querySelector('#groupG');
+    let grpH = document.querySelector('#groupH');
+    grpA.classList.add('active');
+    grpB.classList.remove('active');
+    grpC.classList.remove('active');
+    grpD.classList.remove('active');
+    grpE.classList.remove('active');
+    grpF.classList.remove('active');
+    grpG.classList.remove('active');
+    grpH.classList.remove('active');
+}
+
+
+function ShowGroupB(){
+    let grpA = document.querySelector('#groupA');
+    let grpB = document.querySelector('#groupB');
+    let grpC = document.querySelector('#groupC');
+    let grpD = document.querySelector('#groupD');
+    let grpE = document.querySelector('#groupE');
+    let grpF = document.querySelector('#groupF');
+    let grpG = document.querySelector('#groupG');
+    let grpH = document.querySelector('#groupH');
+    grpA.classList.remove('active');
+    grpB.classList.add('active');
+    grpC.classList.remove('active');
+    grpD.classList.remove('active');
+    grpE.classList.remove('active');
+    grpF.classList.remove('active');
+    grpG.classList.remove('active');
+    grpH.classList.remove('active');
+}
+
+
+function ShowGroupC(){
+    let grpA = document.querySelector('#groupA');
+    let grpB = document.querySelector('#groupB');
+    let grpC = document.querySelector('#groupC');
+    let grpD = document.querySelector('#groupD');
+    let grpE = document.querySelector('#groupE');
+    let grpF = document.querySelector('#groupF');
+    let grpG = document.querySelector('#groupG');
+    let grpH = document.querySelector('#groupH');
+    grpA.classList.remove('active');
+    grpB.classList.remove('active');
+    grpC.classList.add('active');
+    grpD.classList.remove('active');
+    grpE.classList.remove('active');
+    grpF.classList.remove('active');
+    grpG.classList.remove('active');
+    grpH.classList.remove('active');
+}
+
+
+function ShowGroupD(){
+    let grpA = document.querySelector('#groupA');
+    let grpB = document.querySelector('#groupB');
+    let grpC = document.querySelector('#groupC');
+    let grpD = document.querySelector('#groupD');
+    let grpE = document.querySelector('#groupE');
+    let grpF = document.querySelector('#groupF');
+    let grpG = document.querySelector('#groupG');
+    let grpH = document.querySelector('#groupH');
+    grpA.classList.remove('active');
+    grpB.classList.remove('active');
+    grpC.classList.remove('active');
+    grpD.classList.add('active');
+    grpE.classList.remove('active');
+    grpF.classList.remove('active');
+    grpG.classList.remove('active');
+    grpH.classList.remove('active');
+}
+
+
+function ShowGroupE(){
+    let grpA = document.querySelector('#groupA');
+    let grpB = document.querySelector('#groupB');
+    let grpC = document.querySelector('#groupC');
+    let grpD = document.querySelector('#groupD');
+    let grpE = document.querySelector('#groupE');
+    let grpF = document.querySelector('#groupF');
+    let grpG = document.querySelector('#groupG');
+    let grpH = document.querySelector('#groupH');
+    grpA.classList.remove('active');
+    grpB.classList.remove('active');
+    grpC.classList.remove('active');
+    grpD.classList.remove('active');
+    grpE.classList.add('active');
+    grpF.classList.remove('active');
+    grpG.classList.remove('active');
+    grpH.classList.remove('active');
+} 
+
+
+function ShowGroupF(){
+    let grpA = document.querySelector('#groupA');
+    let grpB = document.querySelector('#groupB');
+    let grpC = document.querySelector('#groupC');
+    let grpD = document.querySelector('#groupD');
+    let grpE = document.querySelector('#groupE');
+    let grpF = document.querySelector('#groupF');
+    let grpG = document.querySelector('#groupG');
+    let grpH = document.querySelector('#groupH');
+    grpA.classList.remove('active');
+    grpB.classList.remove('active');
+    grpC.classList.remove('active');
+    grpD.classList.remove('active');
+    grpE.classList.remove('active');
+    grpF.classList.add('active');
+    grpG.classList.remove('active');
+    grpH.classList.remove('active');
+}
+
+
+function ShowGroupG(){
+    let grpA = document.querySelector('#groupA');
+    let grpB = document.querySelector('#groupB');
+    let grpC = document.querySelector('#groupC');
+    let grpD = document.querySelector('#groupD');
+    let grpE = document.querySelector('#groupE');
+    let grpF = document.querySelector('#groupF');
+    let grpG = document.querySelector('#groupG');
+    let grpH = document.querySelector('#groupH');
+    grpA.classList.remove('active');
+    grpB.classList.remove('active');
+    grpC.classList.remove('active');
+    grpD.classList.remove('active');
+    grpE.classList.remove('active');
+    grpF.classList.remove('active');
+    grpG.classList.add('active');
+    grpH.classList.remove('active');
+}
+
+
+function ShowGroupH(){
+    let grpA = document.querySelector('#groupA');
+    let grpB = document.querySelector('#groupB');
+    let grpC = document.querySelector('#groupC');
+    let grpD = document.querySelector('#groupD');
+    let grpE = document.querySelector('#groupE');
+    let grpF = document.querySelector('#groupF');
+    let grpG = document.querySelector('#groupG');
+    let grpH = document.querySelector('#groupH');
+    grpA.classList.remove('active');
+    grpB.classList.remove('active');
+    grpC.classList.remove('active');
+    grpD.classList.remove('active');
+    grpE.classList.remove('active');
+    grpF.classList.remove('active');
+    grpG.classList.remove('active');
+    grpH.classList.add('active');
+}
